2a7f29208d3f4d90cfdd823f483039af4fd59543
Bernard Tison <bernard.tison@gmail.com>
2021-01-05 16:52:27 +0100

    Outbox table with additional columns for CloudEvent metadata

--- ansible/resources/process-service/postgresql/sql/postgresql-outbox.sql
+++ ansible/resources/process-service/postgresql/sql/postgresql-outbox.sql
@@ -9,6 +9,11 @@ CREATE TABLE public.process_service_outbox
     aggregateid character varying(255) COLLATE pg_catalog."default" NOT NULL,
     type character varying(255) COLLATE pg_catalog."default" NOT NULL,
     payload text COLLATE pg_catalog."default",
+    ce_specversion character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    ce_source character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    ce_time character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    ce_datacontenttype character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    ce_incidentid character varying(255) COLLATE pg_catalog."default",
     CONSTRAINT process_service_outbox_pkey PRIMARY KEY (id)
 )
 WITH (
